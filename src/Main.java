50ff81ca1a9e8a940ca371395d5c8e8469afdb53
easterday <easterday@NB4700>
2018-08-23 23:21:54 +0800

    Construct a alg to do step2; We have some problems to fix

--- src/Main.java
+++ src/Main.java
@@ -42,28 +42,96 @@ public class Main {
             step1_a_line(Result1,index,Operand,Operator,InputSize);
         }
     }
-    public static void step2() {
+    private static void ClearZero(int[] dest,int sz) {
+        for (int i=0; i<sz; i++) {
+            dest[i]=0;
+        }
+    }
+    public static void step2_a_line(String line,int[] L,int[] R) {
         final int SZ=InputSize;
-        final int MaxP=SZ-1;
-        int[] L=new int[SZ+1];
-        int[] R=new int[SZ+1];
-        int[] Rinit=new int[SZ+1];
-        L[0]=MaxP; L[1]=0; L[2]=0; L[3]=0;
-        R[0]=0; R[1]=MaxP; R[2]=0; R[3]=0;
-        for (int i=0; i<=SZ; i++) 
-            Rinit[i]=R[i];
-        /*
-        while(L[2]<MaxP) {
-            while(R[3]<MaxP) {
-                
+        final int Slot=SZ+1;        
+        String ret="";
+        for (int i=0,m=0; i<=SZ; i++) {
+            if (i==0) {
+                for (int x=0; x<L[0]; x++) ret+=("(");
+                ret+=(line.charAt(m));
+                m+=2;
+            }else if (i==SZ) {
+                for (int x=0; x<R[Slot-1]; x++) ret+=(")");
+                System.out.println(ret);
+                return;
+            }else {
+                for (int x=0; x<R[i]; x++) ret+=(")");
+                ret+=line.charAt(m-1);
+                for (int x=0; x<L[i]; x++) ret+=("(");
+                ret+=line.charAt(m);
+                m+=2;
             }
         }
-        */
+        System.out.println("BAD_EXECUTION");
+        System.exit(0);
+    }
+    private static boolean Valid(int[] L,int[]  R)  {
+        final int SZ=InputSize;        
+        final int Slot=SZ+1;  
+        if (L[Slot-1]>0) return false;
+        if (R[0]>0) return false;
+        for (int i=0; i<Slot-1; i++) {
+            if (R[i+1]>L[i])    
+                return false;
+            int m=SZ-i;
+            if (L[m-1]>R[m])
+                return false;
+            if (L[i]>0 && R[i+1]>0)
+                return false;
+            if (L[m-1]>0 && R[m]>0)
+                return false;
+        }
+        return true;
+    }
+    public static void step2(String line) {
+        final int SZ=InputSize;
+        final int MaxP=SZ-1;
+        final int Slot=SZ+1;
+        int[] index=new int[MaxP];
+        int[] R=new int[Slot];
+        int[] L=new int[Slot];
+        BigInteger Bmaxp=BigInteger.valueOf(MaxP);        
+        BigInteger BSlot=BigInteger.valueOf(Slot);        
+        BigInteger Limit= BigInteger.valueOf(Slot); Limit=Limit.multiply(Limit);
+        for (BigInteger iter1=BigInteger.ZERO; iter1.compareTo(Limit)==(-1); iter1=iter1.add(BigInteger.ONE)) {            
+            BigInteger Hand1=new BigInteger(iter1.toByteArray());
+            ClearZero(L,MaxP); ClearZero(index,MaxP);
+            for (int i=0; i<MaxP; i++) {
+                index[i]= Hand1.mod(BSlot).intValue();
+                Hand1=Hand1.divide(BSlot);
+            }
+            for (int i=0; i<MaxP; i++) {
+                L[index[i]]++;
+            }
+            for (BigInteger iter2=BigInteger.ZERO; iter2.compareTo(Limit)==(-1); iter2=iter2.add(BigInteger.ONE)) {
+                BigInteger Hand2=new BigInteger(iter2.toByteArray());
+                ClearZero(R,MaxP); ClearZero(index,MaxP);
+                for (int i=0; i<MaxP; i++) {
+                    index[i]=Hand2.mod(BSlot).intValue();
+                    Hand2=Hand2.divide(BSlot);
+                }
+                for (int i=0; i<MaxP; i++) {
+                    R[index[i]]++;
+                }
+                if (Valid(L,R))
+                    step2_a_line(line,L,R);
+            }                        
+        }
+        
+
+        
+        
         
     }
     public static void main(String[] args) {
-        step1();
-        step2();
+        //step1();
+        step2("A+B+C");
     }
             
     
